eaed41372e54d44f78260e34dc1c3867aa5fbcf3
alaa mostafa <alaamostafacs@gmail.com>
2023-09-02 09:50:54 +0300

    add base url

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import { default as axiosroot } from 'axios'
 
 
 const axios = axiosroot.create({
-    baseURL: "http://localhost:3001"
+    baseURL: "https://alef-server.onrender.com"
 })
 
 export default axios;
--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,17 @@ export default defineConfig({
     supported: {
       'top-level-await': true 
     },
+  }, 
+  // added form github link  https://stackoverflow.com/questions/69260715/skipping-larger-chunks-while-running-npm-run-build
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
   }
-})
+});
