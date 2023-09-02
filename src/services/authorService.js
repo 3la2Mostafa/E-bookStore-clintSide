12d3d5fa1d761f710a13416893016da85ad33591
علاء مصطفى <86697971+3la2Mostafa@users.noreply.github.com>
2023-09-02 08:41:31 +0300

    Update authorService.js

--- src/services/authorService.js
+++ src/services/authorService.js
@@ -1,12 +1,15 @@
 import axios from "../config/axiosConfig";
 
 export const getAuthors = async () => {
-  try {
-    const result = await axios.get(`/authors`,{
-    })
-    console.log('result', result.data);
-    return result.data;
+   try {
+    const response = await fetch('https://alef-book-store-server.netlify.app/authors');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log('result', data);
+    return data;
   } catch (error) {
-    console.log('error in getAuthors service -> ', error)
+    console.log('error in getAuthors service -> ', error);
   }
 }
