9d2281549fedc52ed24700c8ae5960b866bf8f18
علاء مصطفى <86697971+3la2Mostafa@users.noreply.github.com>
2023-09-02 08:43:07 +0300

    Update authorService.js

--- src/services/authorService.js
+++ src/services/authorService.js
@@ -2,7 +2,7 @@ import axios from "../config/axiosConfig";
 
 export const getAuthors = async () => {
   try {
-    const result = await axios.get(`https://alef-book-store-server.netlify.app/authors`,{
+    const result = await axios.get(`/authors`,{
     })
     console.log('result', result.data);
     return result.data;
